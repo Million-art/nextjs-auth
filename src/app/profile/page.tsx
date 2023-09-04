319a443ed2affa77e9788a818a71377f9256373e
million-art <millionmulugeta09@gmail.com>
2023-09-04 17:57:34 +0300

    logout action added

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,8 +1,29 @@
-export default function ProfilePage(){
-return(
-    <div>
-        <h1>ProfilePage</h1>
-        <h2>profile</h2>
-    </div>
-)
+"use client"
+import axios from "axios";
+import toast from "react-hot-toast";
+
+export default function ProfilePage() {
+    const logout = async () => {
+        try {
+            const response = await axios.get("/api/users/logout");
+            toast.success("Logout successfully");
+            window.location.href = "/login";
+        } catch (error:any) {
+            toast.error(error.message);
+        }
+    };
+
+    return (
+        <div>
+            <h1>ProfilePage</h1>
+            <h2>Profile</h2>
+            <hr />
+            <button
+                className="text-white bg-red-600 rounded-sm py-2 px-4 font-bold"
+                onClick={logout}
+            >
+                Logout
+            </button>
+        </div>
+    );
 }
